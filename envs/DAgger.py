289a26eb8c61cc5b01ced91f680f511e50a361bd
Pravin Tiwari <pravinkumar2096@gmail.com>
2022-10-20 20:49:59 -0700

    added code to saving demonstrations

---
+++ envs/DAgger.py
@@ -0,0 +1,50 @@
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.ppo import MlpPolicy
+
+env = gym.make("CartPole-v1")
+expert = PPO(
+    policy=MlpPolicy,
+    env=env,
+    seed=0,
+    batch_size=64,
+    ent_coef=0.0,
+    learning_rate=0.0003,
+    n_epochs=10,
+    n_steps=64,
+)
+expert.learn(1000)
+
+
+# Note: set to 100000 to train a proficient expert
+
+
+
+import tempfile
+import gym
+import numpy as np
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+from imitation.algorithms import bc
+from imitation.algorithms.dagger import SimpleDAggerTrainer
+
+venv = DummyVecEnv([lambda: gym.make("CartPole-v1")])
+
+
+bc_trainer = bc.BC(
+    observation_space=env.observation_space,
+    action_space=env.action_space,
+    rng=np.random.default_rng(),
+)
+
+with tempfile.TemporaryDirectory(prefix="dagger_example_") as tmpdir:
+    print(tmpdir)
+    dagger_trainer = SimpleDAggerTrainer(
+        venv=venv,
+        scratch_dir=tmpdir,
+        expert_policy=expert,
+        bc_trainer=bc_trainer,
+        rng=np.random.default_rng(),
+    )
+
+    dagger_trainer.train(2000)
--- envs/GridEnv.py
+++ envs/GridEnv.py
@@ -6,6 +6,8 @@ import sys
 import pygame
 from gym.spaces import Discrete
 import time
+import json
+
 
 
 BLACK = (0, 0, 0)
@@ -14,15 +16,17 @@ RED= (255,0, 0)
 BLUE= (0,0,255)
 GRAY= (128,128,128)
 PINK= (255,105,180)
-colors=[RED, BLUE, PINK]
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 400
+colors=[RED, BLUE, PINK, YELLOW]
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 500
 
 
 class GridEnv(gym.Env):
-    def __init__(self):
+    def __init__(self, block_nums):
+        # Block nums
+        self.block_nums = block_nums
         # blocks order
-        self.final_block_states = [[4,0],[4,1],[4,2]]
+        self.final_block_states = [[4,0],[4,1],[4,2],[4,3]]
         # action space
         self.actions = ['up', 'down', 'right', 'left']
         self.actions_pos_dict = {'up': [-1, 0], 'down': [1, 0], 'right': [0, 1], 'left': [0, -1]}
@@ -34,6 +38,8 @@ class GridEnv(gym.Env):
         self.current_map = copy.deepcopy(self.initial_map)
         self.grid_shape = self.initial_map.shape
 
+        self.current_block = None
+        self.current_block_pos = None
         # agent states
         # self.start_state = self.get_agent_states()
         # self.agent_state = copy.deepcopy(self.start_state)
@@ -41,7 +47,6 @@ class GridEnv(gym.Env):
         # block states
         self.initial_block_states = self.get_block_states()
         self.block_states = copy.deepcopy(self.initial_block_states)
-
 
 
         # env parameters
@@ -56,18 +61,25 @@ class GridEnv(gym.Env):
             return grids
 
     # def get_agent_states(self):
-    #     start_state = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 4)))
+    #     start_state = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 9)))
     #     if start_state == [None, None]:
     #         sys.exit('Start or Target state not specified')
     #     return start_state
 
     def get_block_states(self):
-        block_state1 = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 1)))
-        block_state2 = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 2)))
-        block_state3 = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 3)))
-        if block_state1 == [None, None] or block_state2 == [None, None] or block_state3 == [None, None]:
-            sys.exit('Blocks position not specified')
-        return [block_state1, block_state2, block_state3]
+        block_states = []
+        for i in range(1,self.block_nums+1):
+            block_state = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == i)))
+            if block_state == [None, None]:
+                sys.exit('Blocks position not specified')
+            block_states.append(block_state)
+        return block_states
+        # block_state1 = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 1)))
+        # block_state2 = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 2)))
+        # block_state3 = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == 3)))
+        # if block_state1 == [None, None] or block_state2 == [None, None] or block_state3 == [None, None]:
+        #     sys.exit('Blocks position not specified')
+        # return [block_state1, block_state2, block_state3]
 
     def get_block_state(self, block):
         block_state = list(map(lambda x: x[0] if len(x) > 0 else None, np.where(self.current_map == block)))
@@ -86,8 +98,8 @@ class GridEnv(gym.Env):
         width = self.grid_shape[1]
         block_size = WINDOW_HEIGHT//height
         # blockSize = 100  # Set the size of the grid block
-        for x in range(0, WINDOW_HEIGHT, block_size):
-                for y in range(0, WINDOW_WIDTH, block_size):
+        for x in range(0, WINDOW_WIDTH, block_size):
+                for y in range(0, WINDOW_HEIGHT, block_size):
                     # if self.agent_state[0] * 100 == x and self.agent_state[1] * 100 == y:
                     #     rect = pygame.Rect(self.agent_state[0] * 100, self.agent_state[1] * 100, block_size,
                     #                        block_size)
@@ -97,8 +109,8 @@ class GridEnv(gym.Env):
                     pygame.draw.rect(self.SCREEN, BLACK, rect)
         blocks = self.get_block_states()
         for i in range(len(blocks)):
-            x = blocks[i][0]*100
-            y = blocks[i][1]*100
+            x = blocks[i][1]*100
+            y = blocks[i][0]*100
             # print(x,y)
 
             rect = pygame.Rect(x, y, block_size, block_size)
@@ -120,25 +132,33 @@ class GridEnv(gym.Env):
 
     def step(self, action):
         curr_state = np.sum([self.current_block_pos, self.actions_pos_dict[self.actions[action]]], axis=0)
-
         if curr_state[0] < 0 or curr_state[0] > self.grid_shape[0] - 1 or curr_state[1] < 0 or curr_state[1] > self.grid_shape[1] - 1:
             return self.current_block_pos, 0, False, {}
 
         current_blocks_pos = self.get_block_states()
-        prev_block = current_blocks_pos[((self.current_block - 2) % 3)]
-        next_block = current_blocks_pos[((self.current_block) % 3)]
 
-        if prev_block[0] == curr_state[0] and prev_block[1] == curr_state[1]:
-            return self.current_block_pos, 0, False, {}
-        if next_block[0] == curr_state[0] and next_block[1] == curr_state[1]:
-            return self.current_block_pos, 0, False, {}
+        for i in range(1, self.block_nums + 1):
+            if i == self.current_block:
+                continue
+            block = current_blocks_pos[i - 1]
+            if block[0] == curr_state[0] and block[1] == curr_state[1]:
+                return self.current_block_pos, 0, False, {}
+
+        # prev_block = current_blocks_pos[((self.current_block - 2) % 3)]
+        # next_block = current_blocks_pos[((self.current_block) % 3)]
+        #
+        # if prev_block[0] == curr_state[0] and prev_block[1] == curr_state[1]:
+        #     return self.current_block_pos, 0, False, {}
+        # if next_block[0] == curr_state[0] and next_block[1] == curr_state[1]:
+        #     return self.current_block_pos, 0, False, {}
 
         self.current_map[self.current_block_pos[0], self.current_block_pos[1]] = 0
         self.current_block_pos = curr_state
         self.current_map[self.current_block_pos[0], self.current_block_pos[1]] = self.current_block
-
+        self.block_states = self.get_block_states()
         done = True
         for index, block in enumerate(self.block_states):
+
             if block[0] != self.final_block_states[index][0] or block[1] != self.final_block_states[index][1]:
                 done = False
                 break
@@ -150,7 +170,12 @@ class GridEnv(gym.Env):
         self.current_map = copy.deepcopy(self.initial_map)
         # self.block_states = copy.deepcopy(self.initial_block_states)
 
-env=GridEnv()
+
+
+
+
+
+env=GridEnv(4)
 env.render()
 episodes = 1
 
@@ -166,19 +191,76 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 configurations = os.path.join(file_path, 'configurations.txt')
 final_configurations = read_configurations(configurations)
 
-
-for configuration in final_configurations:
-    for curr_block in configuration:
-        env.current_block = curr_block
-        env.current_block_pos = env.get_block_state(curr_block)
-        done = False
-        for i in range(10):
-            time.sleep(1)
-            # env.render()
-            action = env.action_space.sample()
-            n_state, reward, done, info = env.step(action)
+done = False
+data = {
+    "final_configurations": final_configurations.tolist(),
+    "data" : []
+}
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = list(pygame.mouse.get_pos())
+            y = pos[0] // 100
+            x = pos[1] // 100
+            block_states = env.get_block_states()
+            for index, block in enumerate(block_states):
+                if x == block[0] and y == block[1]:
+                    env.current_block = index + 1
+                    env.current_block_pos = env.get_block_state(env.current_block)
+                    data["data"].append({
+                        "current_block": env.current_block,
+                        "current_block_moves" : [],
+                        "grid_states":[]
+                    })
+                    break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                data["data"][-1]["current_block_moves"].append(3)
+                data["data"][-1]["grid_states"].append(env.current_map.tolist())
+                n_state, reward, done, info = env.step(3)
+            if event.key == pygame.K_RIGHT:
+                data["data"][-1]["current_block_moves"].append(2)
+                data["data"][-1]["grid_states"].append(env.current_map.tolist())
+                n_state, reward, done, info = env.step(2)
+            if event.key == pygame.K_UP:
+                data["data"][-1]["current_block_moves"].append(0)
+                data["data"][-1]["grid_states"].append(env.current_map.tolist())
+                n_state, reward, done, info = env.step(0)
+            if event.key == pygame.K_DOWN:
+                data["data"][-1]["current_block_moves"].append(1)
+                data["data"][-1]["grid_states"].append(env.current_map.tolist())
+                n_state, reward, done, info = env.step(1)
             env.drawGrid()
             env.update()
+
+
+print(data)
+# Serializing json
+json_object = json.dumps(data, indent=4)
+# Writing to demonstration.json
+with open("demonstration.json", "w") as outfile:
+    outfile.write(json_object)
+
+# for configuration in final_configurations:
+#     for curr_block in configuration:
+#         env.current_block = curr_block
+#         env.current_block_pos = env.get_block_state(curr_block)
+#         done = False
+#         for i in range(10):
+#             time.sleep(1)
+#             action = env.action_space.sample()
+#             n_state, reward, done, info = env.step(action)
+#             if done:
+#                 sys.exit("Success")
+#             env.drawGrid()
+#             env.update()
             # print(env.actions[action])
             # print(env.current_map)
+
+
+
+
+
 
